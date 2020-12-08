f8d240f71b196e45ed4a4a0763c540399a238641
Mikhail Beliy <misha.beliy15@gmail.com>
2020-12-08 19:54:32 +0200

    Initial Commit

---
+++ AI-Insurance/AI_InsuranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_InsuranceApp.swift
+//  AI-Insurance
+//
+//  Created by Misha Beliy on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AI_InsuranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-Insurance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AI-Insurance
+//
+//  Created by Misha Beliy on 08.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
